ee7a06daa40f319e74c831c0a4cad134bf8c7a06
josepablovr <josepablo2106@gmail.com>
2021-08-09 23:21:31 -0600

    Version 1.1
    Código de error agregado, errores de flake 8 corregidos.  Penidiente: documentacion y terminar la función test con los datos de la otra función

--- multiple_op.py
+++ multiple_op.py
@@ -1,13 +1,13 @@
 def multiple_op(X):
     import math
     resultado = [0, 0, 0]
-    try:
-        str(X).isnumeric()
+    ERROR_BAD_ARGUMENTS = 0xA0
+    if str(X).isnumeric() is True and X >= 0:
         resultado[0] = X * X
         resultado[1] = 2**X
         resultado[2] = math.factorial(X)
         return resultado
 
-    except:
+    else:
         print("El dato ingresado no es valido")
-        return -1
+        return ERROR_BAD_ARGUMENTS
--- test_funciones.py
+++ test_funciones.py
@@ -3,9 +3,9 @@ from multiple_op import multiple_op
 
 def test_funciones():
     import math
-    import random    
+    import random
     n = random.randint(0, 100000)
-    Negativo1 = "v"
+    Negativo1 = "w"
 
     assert multiple_op(n) == [math.prod([n, n]), pow(2, n), math.factorial(n)]
-    assert multiple_op(Negativo1) == -1
+    assert multiple_op(Negativo1) == 0xA0
